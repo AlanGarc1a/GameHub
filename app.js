a28d1a06c43d63b3579d89d643505b27ece8da62
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-03-10 14:09:11 -0800

    added mongodb connection and gamecard route

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const express = require('express');
+const express    = require('express');
+const mongoose   = require('mongoose');
+const cors       = require('cors');
+const bodyParser = require('body-parser');
+
 const app     = express();
 const port    = 5000;
+
+//game routes
+const gameRoutes = require('./routes/gameRoute');
+const { urlencoded } = require('express');
+
+mongoose.connect('mongodb://localhost:27017/gamehub', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connection open');
+}).catch( err => {
+    console.log("MongoDB connection failed");
+    console.log(err);
+});
+
+app.use(cors());
+app.use(urlencoded({ extended: true }));
+
+//game routes
+app.use('/g', gameRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
