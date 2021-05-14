bcdc8c170f0353679e73e0935d895f8632e7d9f2
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-05-14 16:26:15 -0700

    added a middleware and a function to the user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
     username: {
         type: String,
@@ -8,6 +10,7 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true
     },
     password: {
@@ -15,6 +18,25 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function(next){
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch(error) {
+        next(error);
+    }
+});
+
+userSchema.methods.isValidPassword = async function(password) {
+    try {
+       return await bcrypt.compare(password, this.password);
+    } catch(error) {
+        throw new Error('Invalid Error');
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 
