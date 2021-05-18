820c6b6fb90252315010fa7f5697a82f90794bc3
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-05-18 14:55:29 -0700

    added the context provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 import GameCardCreate from './games/GameCardCreate';
 import GameCardEdit from './games/GameCardEdit';
@@ -9,43 +9,57 @@ import Register from './Register';
 import Login from './Login';
 import NotFound from './NotFound';
 import AuthContext from './store/AuthContext';
+import axios from 'axios';
 
-const App = () => {
+function App() {
 
-    const authCntx = useContext(AuthContext);
-    const isLoggedIn = authCntx.isLoggedIn;
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined,
+    });
+
+    useEffect(() => {
+
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem("auth-token");
+
+            if (token === null) {
+                localStorage.setItem("auth-token", "");
+                token = "";
+            }
+
+            const tokenResponse = await axios.post('http://localhost:5000/u/tokenIsValid', null, { headers: {"x-auth-token": token} });
+            
+            if (tokenResponse.data) {
+                const userRes = await axios.get('http://localhost:5000/u/', {
+                    headers: { "x-auth-token": token },
+                });
+
+                setUserData({
+                    token,
+                    user: userRes.data,
+                });
+            }
+        }
+
+        checkLoggedIn();
+    }, []);
 
     return (
-        <div>
-            <NavBar />
-            <div>
+        <BrowserRouter>
+            <AuthContext.Provider value={{ userData, setUserData }}>
+                <NavBar />
                 <Switch>
-                    {isLoggedIn && (
-                        <Route path="/" exact component={Home} />
-                    )}
-
-                    {isLoggedIn && (
-                        <Route path="/create" exact component={GameCardCreate} />
-                    )}
-
-                    {isLoggedIn && (
-                        <Route path="/edit/:id" exact component={GameCardEdit} />
-                    )}
-
-                    {isLoggedIn && (
-                        <Route path="/:title/:id" exact component={GameCardView} />
-                    )}
-                    {!isLoggedIn && (
-                        <Route path="/register" exact component={Register} />
-                    )}
-
-                    {!isLoggedIn && (
-                        <Route path="/login" exact component={Login} />
-                    )}
+                    <Route path="/" exact component={Home} />
+                    <Route path="/create" exact component={GameCardCreate} />
+                    <Route path="/edit/:id" exact component={GameCardEdit} />
+                    <Route path="/:title/:id" exact component={GameCardView} />
+                    <Route path="/register" exact component={Register} />
+                    <Route path="/login" exact component={Login} />
                     <Route path="*" exact component={NotFound} />
                 </Switch>
-            </div>
-        </div>
+            </AuthContext.Provider>
+        </BrowserRouter>
     );
 
 };
