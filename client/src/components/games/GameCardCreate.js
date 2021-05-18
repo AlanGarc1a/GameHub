3ee62836f5217ca25d7e7cb8c515ceb675623812
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-05-18 14:53:32 -0700

    changed from a class to a function component

--- client/src/components/games/GameCardCreate.js
+++ client/src/components/games/GameCardCreate.js
@@ -1,223 +1,196 @@
-import React from "react";
-import { Grid, Button, TextField, withStyles, Typography, Paper } from "@material-ui/core";
+import { useState, useContext } from "react";
+import { Grid, Button, TextField, makeStyles, Typography, Paper, Container } from "@material-ui/core";
 import { Link, Redirect } from "react-router-dom";
+import AuthContext from '../store/AuthContext';
 import axios from "axios";
 
-const styles = (theme) => ({
-    form: {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '500px'
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        paddingTop: '100px',
+        paddingBottom: '100px',
+        paddingLeft: '85px',
+        paddingRight: '85px',
+        width: '30%'
     },
-    formTitle: {
-        textAlign: 'center',
-        padding: '25px 0px 25px 0px'
+    title: {
+        marginBottom: theme.spacing(3)
     },
     textField: {
-        marginBottom: theme.spacing(1),
-        marginLeft: theme.spacing(5),
-        marginRight: theme.spacing(5),
+        marginBottom: theme.spacing(3)
     },
-    textFieldArea: {
-        marginBottom: theme.spacing(5),
-        marginLeft: theme.spacing(5),
-        marginRight: theme.spacing(5),
-    },
-    submitButton: {
-        margin: '0px 0px 50px 50px',
-        //width: '100px'
-    },
-    cancelButton: {
-        margin: '0px 0px 50px 25px'
+    button: {
+        marginRight: theme.spacing(3),
     }
-});
+}));
 
-class GameCardCreate extends React.Component {
+const GameCardCreate = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            titleError: "",
-            image: "",
-            imageError: "",
-            date: "",
-            dateError: "",
-            body: "",
-            bodyError: "",
-            redirect: false,
-            submitted: false
-        };
-    }
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+    const [date, setDate] = useState('');
+    const [body, setBody] = useState('');
 
-    validate = () => {
-        let isError = false;
+    const [titleError, setTitleError] = useState(false);
+    const [dateError, setDateError] = useState(false);
+    const [imageError, setImageError] = useState(false);
+    const [bodyError, setBodyError] = useState(false);
+    const [redirect, setRedirect] = useState(false);
 
-        const errors = {
-            titleError: "",
-            dateError: "",
-            imageError: "",
-            bodyError: "",
-            errorTitle: false,
-            errorDate: false,
-            errorImage: false,
-            errorBody: false,
-        };
+    const { userData } = useContext(AuthContext);
 
-        if (this.state.title === "") {
-            isError = true;
-            errors.titleError = "Cannot have empty title";
-            errors.errorTitle = true;
-        }
-        if (this.state.date === "") {
-            isError = true;
-            errors.dateError = "Cannot have empty date";
-            errors.errorDate = true;
-        }
-        if (this.state.image === "") {
-            isError = true;
-            errors.imageError = "Cannot have empty image";
-            errors.errorImage = true;
-        }
-        if (this.state.body === "") {
-            isError = true;
-            errors.bodyError = "Cannot have empty body";
-            errors.errorBody = true;
-        }
-
-        this.setState({ ...this.state, ...errors });
-
-        return isError;
+    const titleHandler = (event) => {
+        event.persist();
+        setTitle(event.target.value);
     };
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        });
-    };
+    const imageHandler = (event) => {
+        event.persist();
+        setImage(event.target.value);
+    }
 
-    onSubmit = (event) => {
+    const dateHandler = (event) => {
+        event.persist();
+        setDate(event.target.value);
+    }
+
+    const bodyHandler = (event) => {
+        event.persist();
+        setBody(event.target.value);
+    }
+
+    const onSubmitHandler = (event) => {
         event.preventDefault();
 
-        const error = this.validate();
-
         const game = {
-            title: this.state.title,
-            date: this.state.date,
-            image: this.state.image,
-            body: this.state.body,
+            title: title,
+            date: date,
+            image: image,
+            body: body,
         };
 
-        if (error) {
-            this.setState({
-                title: "",
-                titleError: "",
-                date: "",
-                dateError: "",
-                image: "",
-                imageError: "",
-                body: "",
-                bodyError: ""
-            });
+        if (title === '') {
+            setTitleError(true);
+        }
+        if (date === '') {
+            setDateError(true);
+        }
+        if (image === '') {
+            setImageError(true);
+        }
+        if (body === '') {
+            setBodyError(true);
         }
         else {
             axios.post("http://localhost:5000/g/create", game)
                 .then(() => {
-                    this.setState({ redirect: true, submitted: true });
+                    setRedirect(true);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
-
-            this.setState({
-                title: "",
-                date: "",
-                image: "",
-                body: "",
-            });
         }
     };
 
-    render() {
-        const { classes } = this.props;
-        const { redirect, submitted, title, date, image, body } = this.state;
+    const classes = useStyles();
 
-        if (redirect && submitted) {
-            return <Redirect to="/" />;
-        }
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
 
-        return (
-            <div>
-                <Grid container direction="column" justify="center" alignItems="center" style={{ minHeight: '95vh' }}>
-                    <Grid item xs={12} sm={6}>
-                        <Paper variant="elevation" elevation={3}>
-                            <Typography variant="h5" className={classes.formTitle}>
-                                New Game
-                            </Typography>
-                            <form onSubmit={this.onSubmit} className={classes.form}>
-                                <TextField
-                                    className={classes.textField}
-                                    label="Title"
-                                    variant="filled"
-                                    size="small"
-                                    error={this.state.errorTitle}
-                                    helperText={this.state.titleError}
-                                    name="title"
-                                    value={title}
-                                    onChange={this.handleChange}
-                                />
-                                <TextField
-                                    className={classes.textField}
-                                    label="Date"
-                                    variant="filled"
-                                    size="small"
-                                    error={this.state.errorDate}
-                                    helperText={this.state.dateError}
-                                    name="date"
-                                    value={date}
-                                    onChange={this.handleChange}
-                                />
-                                <TextField
-                                    className={classes.textField}
-                                    label="Image"
-                                    variant="filled"
-                                    size="small"
-                                    error={this.state.errorImage}
-                                    helperText={this.state.imageError}
-                                    name="image"
-                                    value={image}
-                                    onChange={this.handleChange}
-                                />
-                                <TextField
-                                    className={classes.textFieldArea}
-                                    label="Summary"
-                                    variant="filled"
-                                    size="small"
-                                    error={this.state.errorBody}
-                                    helperText={this.state.bodyError}
-                                    multiline
-                                    rows={8}
-                                    name="body"
-                                    value={body}
-                                    onChange={this.handleChange}
-                                />
-                                <div>
-                                    <Button variant="contained" color="primary" type="submit" className={classes.submitButton}>
+    return (
+        <div>
+            { userData.user ?
+                <Container style={{ minHeight: '95vh' }}>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                        style={{ minHeight: '95vh' }}
+                    >
+                        <Paper variant="elevation" elevation={6} className={classes.paper}>
+                            <Grid item>
+                                <Typography variant="h5" align="center" className={classes.title}>
+                                    New Game
+                                </Typography>
+                            </Grid>
+                            <form onSubmit={onSubmitHandler}>
+                                <Grid item>
+                                    <TextField
+                                        label="Title"
+                                        variant="filled"
+                                        size="small"
+                                        error={titleError}
+                                        name="title"
+                                        value={title}
+                                        onChange={titleHandler}
+                                        fullWidth
+                                        className={classes.textField}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        label="Date"
+                                        variant="filled"
+                                        size="small"
+                                        error={dateError}
+                                        name="date"
+                                        value={date}
+                                        onChange={dateHandler}
+                                        fullWidth
+                                        className={classes.textField}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        label="Image"
+                                        variant="filled"
+                                        size="small"
+                                        error={imageError}
+                                        name="image"
+                                        value={image}
+                                        onChange={imageHandler}
+                                        fullWidth
+                                        className={classes.textField}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        label="Summary"
+                                        variant="filled"
+                                        size="small"
+                                        error={bodyError}
+                                        multiline
+                                        rows={8}
+                                        name="body"
+                                        value={body}
+                                        onChange={bodyHandler}
+                                        fullWidth
+                                        className={classes.textField}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained" color="primary" type="submit" className={classes.button}>
                                         Submit
                                     </Button>
                                     <Link to="/">
-                                        <Button variant="contained" color="secondary" className={classes.cancelButton}>
+                                        <Button variant="contained" color="default">
                                             Cancel
                                         </Button>
                                     </Link>
-                                </div>
+                                </Grid>
                             </form>
                         </Paper>
                     </Grid>
+                </Container> :
+                <Grid container justify="center" alignItems="center" style={{ minHeight: '95vh' }}>
+                    <Typography variant="h4">
+                        You must be logged in to create a Game.
+                    </Typography>
                 </Grid>
-            </div>
-        );
-    }
+            }
+        </div>
+    );
 }
 
-export default withStyles(styles)(GameCardCreate);
+export default GameCardCreate;
