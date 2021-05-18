9ee28696f4dcd876e2c5b10818b895ed1ced402e
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-05-18 11:39:55 -0700

    made sure the user is logged in before seeing their saved games

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import AuthContext from './store/AuthContext';
 import GameList from './games/GameList';
+import { Typography, Grid } from '@material-ui/core';
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <GameList />
-            </div>
-        );
-    }
+const Home = () => {
+    const { userData } = useContext(AuthContext);
+
+    return (
+        <div>
+            {
+                userData.user ? <GameList /> :
+                    <Grid container justify="center" alignItems="center" style={{ minHeight: '95vh' }}>
+                        <Grid item>
+                            <Typography variant="h4" align="center">
+                                You must be logged in to see your saved Games.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+            }
+        </div>
+    );
+
 }
 
 export default Home;
