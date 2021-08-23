87b6294b1d87d79807e3418d16449ba45cd7efb6
AlanGarc1a <alangarcia969@gmail.com>
2021-08-23 09:12:49 -0700

    fixed url endpoint

--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -4,22 +4,22 @@ const router  = express.Router();
 const gameController = require('../controllers/gamecardcontroller');
 
 //get all games
-//endpoint: http://localhost:5000/g/
+//endpoint: http://localhost:5000/api/games/
 router.get('/', gameController.index);
 
-//endpoint: http://localhost:5000/g/create
+//endpoint: http://localhost:5000/api/games/create
 //create a game
 router.post('/create', gameController.create);
 
-//endpoint: http://localhost:5000/g/:id
+//endpoint: http://localhost:5000/api/games/:id
 //get one game
 router.get('/:id', gameController.view);
 
-//endpoint: http://localhost:5000/g/update/:id
+//endpoint: http://localhost:5000/api/games/update/:id
 //update one game
 router.put('/update/:id', gameController.update);
 
-//ednpoint: http://localhost:5000/g/delete-game/:id
+//ednpoint: http://localhost:5000/api/games/delete/:id
 //delete one game
 router.delete('/delete/:id', gameController.delete);
 
