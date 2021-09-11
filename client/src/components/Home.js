ccf5b371d9c5af5996925667cc61c451bba5e206
AlanGarc1a <alangarcia969@gmail.com>
2021-09-10 20:25:55 -0700

    import header into home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import AuthContext from './store/AuthContext';
 import GameList from './games/GameList';
-import { Typography, Grid } from '@material-ui/core';
+import Header from './Header';
 
 const Home = () => {
     const { userData } = useContext(AuthContext);
@@ -9,14 +9,7 @@ const Home = () => {
     return (
         <div>
             {
-                userData.user ? <GameList /> :
-                    <Grid container justify="center" alignItems="center" style={{ minHeight: '95vh' }}>
-                        <Grid item>
-                            <Typography variant="h4" align="center">
-                                You must be logged in to see your saved Games.
-                            </Typography>
-                        </Grid>
-                    </Grid>
+                userData.user ? <GameList /> : <Header />
             }
         </div>
     );
