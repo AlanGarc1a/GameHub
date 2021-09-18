7a4ded44f9d8c3a140e71b842fe164bd17bae316
AlanGarc1a <alangarcia969@gmail.com>
2021-09-17 23:07:34 -0700

    changed class component to function component

--- client/src/components/games/GameCard.js
+++ client/src/components/games/GameCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Button, Card, CardActions, CardContent, CardHeader, withStyles, Typography } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardHeader, makeStyles, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const styles = theme => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 350,
         height: '100%'
@@ -19,40 +19,36 @@ const styles = theme => ({
     cardContent: {
         marginBottom: '20px'
     }
-});
+}));
 
-class GameCard extends React.Component {
+const GameCard = (props) => {
 
-    render() {
-        const { classes } = this.props;
-        const { id, title, date, summary } = this.props;
+    const classes  = useStyles();
+    const { id, title, date, summary } = props;
 
-        return (
-            <div>
-                <Card className={classes.root}>
-                    <CardHeader
-                        title={title}
-                        subheader={date}
-                    />
-                    <CardContent className={classes.cardContent}>
-                        <div className={classes.body}>
-                            <Typography component="p" className={classes.content}>
-                                {summary}
-                            </Typography>
-                        </div>
-                    </CardContent>
-                    <CardActions>
-                        <Link to={`/edit/${id}`}>
-                            <Button color="primary">Edit</Button>
-                        </Link>
-                        <Link to={`/${title}/${id}`}>
-                            <Button color="primary">View</Button>
-                        </Link>
-                    </CardActions>
-                </Card>
-            </div>
-        );
-    }
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                title={title}
+                subheader={date}
+            />
+            <CardContent className={classes.cardContent}>
+                <div className={classes.body}>
+                    <Typography component="p" className={classes.content}>
+                        {summary}
+                    </Typography>
+                </div>
+            </CardContent>
+                <CardActions>
+                <Link to={`/edit/${id}`}>
+                    <Button color="primary">Edit</Button>
+                </Link>
+                <Link to={`/${title}/${id}`}>
+                    <Button color="primary">View</Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
 }
 
-export default withStyles(styles)(GameCard);
+export default GameCard;
